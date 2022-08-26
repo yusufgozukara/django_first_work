010ca15baab3dbc318b839a8568d8a368e504902
yusufgozukara <yusufgozukara11@gmail.com>
2022-08-26 10:19:52 +0300

    context added

--- fsApp/views.py
+++ fsApp/views.py
@@ -7,4 +7,9 @@ from django.http import HttpResponse
 #     return HttpResponse('Hi')
 
 def home(request):
-    return render(request, 'fsApp/index.html')
+    context = {
+        'caption': 'clarusway',
+        'dict1': {'django': 'best framework'},
+        'my_list': [2, 3, 4]
+    }
+    return render(request, 'fsApp/index.html', context)
